67282b92bec5b727617b7acf7b2ba1df1234e4ba
NoelHer <crisherper52@gmail.com>
2023-12-08 10:07:22 -0600

    Module not found: Error Can't resolve

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import './index.css';
-import { Header } from "./componentes/Header/Index";
+import  Header  from "./componentes/Header/Index";
 import 'boxicons';
 import { BrowserRouter as Router } from "react-router-dom";
-import { Pagina } from "./componentes/Pagina/Pagina";
+import  Pagina  from "./componentes/Pagina/Pagina";
 
 function App() {
   return (
